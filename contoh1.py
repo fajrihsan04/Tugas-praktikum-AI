6337765813306b04c02839d57e8323ee8ce04b25
fajrihsan04 <126737925+fajrihsan04@users.noreply.github.com>
2023-03-02 09:20:50 +0700

    Add files via upload

---
+++ contoh1.py
@@ -0,0 +1,6 @@
+angka1 = input('Tulis angka pertama: ')
+angka2 = input('Tulis angka kedua: ')
+ 
+angka3 = int(angka1) + int(angka2)
+ 
+print('Hasil Penjumlahan {0} dan {1} adalah {2}'.format(angka1, angka2, angka3))
---
+++ contoh2.py
@@ -0,0 +1,7 @@
+angka = int(input("Tulis sebuah Angka: "))
+
+if (angka % 2) == 0:
+   print("{0} adalah Bilangan Genap".format(angka))
+
+else:
+   print("{0} adalah Bilangan Ganjil".format(angka))
